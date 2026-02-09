96d5924596d9615842ee06156e96ba2073391011
Aqib2110 <muhammadaqib2110@gmail.com>
2026-02-09 22:30:04 +0500

    updated

--- lib/queue.ts
+++ lib/queue.ts
@@ -3,8 +3,8 @@ import { Redis } from 'ioredis';
 
 const connection = new Redis({
   host: process.env.REDIS_HOST,
-  port: 20561,
-  username: 'default',
+  port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME,
   password: process.env.REDIS_PASSWORD,
   tls: {}, 
   maxRetriesPerRequest: null
