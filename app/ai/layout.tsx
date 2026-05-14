04a07ef28e326fff25e67bac11abb60dd82ae931
Aqib2110 <muhammadaqib2110@gmail.com>
2026-05-14 22:12:50 +0500

    updated AI page

---
+++ app/ai/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import Navbar from "../components/Navbar"
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Personal Knowledge Manager",
+  description: "Organize, store, and retrieve your personal knowledge base efficiently."
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="flex flex-col bg-gray-100 w-full">
+          <Navbar />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
